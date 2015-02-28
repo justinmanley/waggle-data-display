fb729e04193bfc5d7394e3489a7698d0f0727c6b
Justin Manley <manleyjster@gmail.com>
2015-02-28 13:05:16 -0600

    Finish parsing all of the sensor output streams.

--- Parse.elm
+++ Parse.elm
@@ -33,17 +33,65 @@ parseSensor s =
             }
         "MMA8452Q.Freescale.8_1-2013" ->
             Just <| MMA8452Q { basic | 
-                acceleration = parseAcceleration (data |> get 2) (data |> get 3) 
+                acceleration = parseCompoundDataPoint (data |> get 2) (data |> get 3) 
             }
         "Thermistor_NTC_PR103J2.US_Sensor.2003" ->
-            Just <| PR103J2 { 
-                basic | temperature = data |> get 2 |> parseSimpleDataPoint 
+            Just <| PR103J2 { basic | 
+                temperature = data |> get 2 |> parseSimpleDataPoint 
             }
         "HIH6130.Honeywell.2011" -> 
             let temp = { basic | temperature = data |> get 2 |> parseSimpleDataPoint }
-            in Just <| HIH6130 { 
-                temp | humidity = data |> get 3 |> parseSimpleDataPoint 
+            in Just <| HIH6130 { temp | 
+                humidity = data |> get 3 |> parseSimpleDataPoint 
             }
+        "GA1A1S201WP.Sharp.2007" -> 
+            Just <| GA1A1S201WP { basic | 
+                luminousIntensity = data |> get 2 |> parseSimpleDataPoint
+            }
+        "MAX4466.Maxim.2001" -> 
+            Just <| MAX4466 { basic | 
+                acousticIntensity = data |> get 2 |> parseSimpleDataPoint
+            }
+        "D6T-44L-06.Omron.2012" ->
+            Just <| D6T44L06 { basic | 
+                temperatures = map parseSimpleDataPoint (drop 2 data)
+            }
+        "HMC5883.Honeywell.2013" -> 
+            Just <| HMC5883 { basic | 
+                magneticField = parseCompoundDataPoint (data |> get 2) (data |> get 3)
+            }
+        "SHT15.Sensirion.4_3-2010" ->
+            let temp = { basic | temperature = data |> get 2 |> parseSimpleDataPoint }
+            in Just <| SHT15 { temp | 
+                humidity = data |> get 3 |> parseSimpleDataPoint
+            }
+        "HTU21D.MeasSpec.2013" ->
+            let temp = { basic | temperature = data |> get 2 |> parseSimpleDataPoint }
+            in Just <| HTU21D { temp | 
+                humidity = data |> get 3 |> parseSimpleDataPoint
+            }
+        "DS18B20.Maxim.2008" ->
+            Just <| DS18B20 { basic | 
+                temperature = data |> get 2 |> parseSimpleDataPoint
+            }
+        "RHT03.Maxdetect.2011" ->
+            let temp = { basic | temperature = data |> get 2 |> parseSimpleDataPoint }
+            in Just <| RHT03 { temp | 
+                humidity = data |> get 3 |> parseSimpleDataPoint
+            }
+        "TMP102.Texas_Instruments.2008" ->
+            Just <| TMP102 { basic | 
+                temperature = data |> get 2 |> parseSimpleDataPoint
+            }
+        "SHT75.Sensirion.5_2011" -> 
+            let temp = { basic | temperature = data |> get 2 |> parseSimpleDataPoint }
+            in Just <| SHT75 { temp | 
+                humidity = data |> get 3 |> parseSimpleDataPoint
+            }
+        "HIH4030.Honeywell.2008" ->
+            Just <| HIH4030 { basic | 
+                humidity = data |> get 2 |> parseSimpleDataPoint
+            }     
         _ -> Nothing
 
 parseBasicSensor : List String -> { name : String, timestamp : String }
@@ -56,19 +104,21 @@ parseSimpleDataPoint s =
         { value = data |> get 1 |> String.toFloat |> toMaybe,
           units = data |> get 2 }
 
-parseAcceleration : String -> String -> { x : Acceleration, y : Acceleration }
-parseAcceleration x y = 
-    let accelerationX = String.split ";" x
-        accelerationY = String.split ";" y
+parseCompoundDataPoint : String 
+    -> String 
+    -> { x : { value : Maybe Float, units : String }, y : { value : Maybe Float, units : String } }
+parseCompoundDataPoint x y = 
+    let xs = String.split ";" x
+        ys = String.split ";" y
     in
         { 
             x = { 
-                value = accelerationX |> get 1 |> String.toFloat |> toMaybe,
-                units = accelerationX |> get 2
+                value = xs |> get 1 |> String.toFloat |> toMaybe,
+                units = xs |> get 2
             },
             y = {
-                value = accelerationY |> get 1 |> String.toFloat |> toMaybe,
-                units = accelerationY |> get 2
+                value = ys |> get 1 |> String.toFloat |> toMaybe,
+                units = ys |> get 2
             }
         }
 
