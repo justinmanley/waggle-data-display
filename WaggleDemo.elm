210cb9a329d6469f8ff295ae73d8157f9ade4bdc
Justin Manley <manleyjster@gmail.com>
2015-03-01 18:03:34 -0600

    Position each data point next to its originating sensor in the envsense image.

--- WaggleDemo.elm
+++ WaggleDemo.elm
@@ -5,10 +5,10 @@ import Signal
 import Http
 import Time (Time, every, second)
 import Window
-import Text (leftAligned, fromString, color, asText)
+import Text (leftAligned, rightAligned, fromString, color, asText)
 import Color (red)
 import Graphics.Element (
-    Element, 
+    Element, Position,
     flow, layers,
     image, container,
     down, right, middle, outward, inward, middleAt, midTop,  midLeft, midRightAt, midLeftAt,
@@ -47,68 +47,88 @@ view : (Int, Int) -> List (Maybe Sensor.SensorData) -> Element
 view (windowWidth, windowHeight) data = 
     let 
         innerWidth = min 980 windowWidth
-        sensorImage = image 306 406 "http://localhost:8000/assets/env-sense-annotated.png"
+        (imageWidth, imageHeight) = (306, 406)
+        sensorImage = image imageWidth imageHeight "http://localhost:8000/assets/env-sense-annotated.png"
     in
         layers [
             container windowWidth windowHeight middle sensorImage,
             flow inward
                 <| List.map (container windowWidth windowHeight middle)
-                <| List.map (viewSensor windowWidth windowHeight) data
+                <| List.map (viewSensor (windowWidth, windowHeight) (imageWidth, imageHeight)) data
         ]
 
-viewSensor : Int -> Int -> Maybe Sensor.SensorData -> Element
-viewSensor windowWidth windowHeight maybeSensor = case maybeSensor of 
+viewSensor : (Int, Int) -> (Int, Int) -> Maybe Sensor.SensorData -> Element
+viewSensor (windowWidth, windowHeight) imageDimensions maybeSensor = case maybeSensor of 
     Just sensor -> 
-        let e = case sensor of
-            Sensor.MLX90614ESF { temperature, timestamp, name } -> viewBasic temperature
-            Sensor.TMP421 { temperature, timestamp, name } -> viewBasic temperature
-            Sensor.BMP180 { temperature, pressure, timestamp, name } -> flow down
-                <| List.map viewBasic [temperature, pressure]
-            Sensor.MMA8452Q { acceleration, timestamp, name } -> viewPoint acceleration
-            Sensor.PDV_P8104 { luminousIntensity, timestamp, name } -> viewBasic luminousIntensity
-            Sensor.PR103J2 { temperature, timestamp, name } -> viewBasic temperature
-            Sensor.HIH6130 { temperature, humidity, timestamp, name } -> flow right 
-                <| List.map viewBasic [temperature, humidity]
-            Sensor.SHT15 { temperature, humidity, timestamp, name } -> flow down
-                <| List.map viewBasic [temperature, humidity]
-            Sensor.HTU21D { temperature, humidity, timestamp, name } -> flow right 
-                <| List.map viewBasic [temperature, humidity]
-            Sensor.DS18B20 { temperature, timestamp, name } -> viewBasic temperature
-            Sensor.RHT03 { temperature, humidity, timestamp, name } -> flow down
-                <| List.map viewBasic [temperature, humidity]
-            Sensor.TMP102 { temperature, timestamp, name } -> viewBasic temperature
-            Sensor.SHT75 { temperature, humidity, timestamp, name } -> flow right 
-                <| List.map viewBasic [temperature, humidity]
-            Sensor.HIH4030 { humidity, timestamp, name } -> viewBasic humidity
-            Sensor.GA1A1S201WP { luminousIntensity, timestamp, name } -> viewBasic luminousIntensity
-            Sensor.MAX4466 { acousticIntensity, timestamp, name } -> viewBasic acousticIntensity
-            Sensor.D6T44L06 { temperatures, timestamp, name } -> flow right 
-                <| List.map viewBasic temperatures
-            Sensor.HMC5883 { magneticField, timestamp, name } -> viewPoint magneticField
+        let viewBasic' = viewBasic (side sensor)
+            viewPoint' = viewPoint (side sensor)
+            element = case sensor of
+                Sensor.MLX90614ESF { temperature, timestamp, name } -> viewBasic' temperature
+                Sensor.TMP421 { temperature, timestamp, name } -> viewBasic' temperature
+                Sensor.BMP180 { temperature, pressure, timestamp, name } -> flow down
+                    <| List.map viewBasic' [temperature, pressure]
+                Sensor.MMA8452Q { acceleration, timestamp, name } -> viewPoint' acceleration
+                Sensor.PDV_P8104 { luminousIntensity, timestamp, name } -> viewBasic' luminousIntensity
+                Sensor.PR103J2 { temperature, timestamp, name } -> viewBasic' temperature
+                Sensor.HIH6130 { temperature, humidity, timestamp, name } -> flow down 
+                    <| List.map viewBasic' [temperature, humidity]
+                Sensor.SHT15 { temperature, humidity, timestamp, name } -> flow down
+                    <| List.map viewBasic' [temperature, humidity]
+                Sensor.HTU21D { temperature, humidity, timestamp, name } -> flow right 
+                    <| List.map viewBasic' [temperature, humidity]
+                Sensor.DS18B20 { temperature, timestamp, name } -> viewBasic' temperature
+                Sensor.RHT03 { temperature, humidity, timestamp, name } -> flow down
+                    <| List.map viewBasic' [temperature, humidity]
+                Sensor.TMP102 { temperature, timestamp, name } -> viewBasic' temperature
+                Sensor.SHT75 { temperature, humidity, timestamp, name } -> flow right 
+                    <| List.map viewBasic' [temperature, humidity]
+                Sensor.HIH4030 { humidity, timestamp, name } -> viewBasic' humidity
+                Sensor.GA1A1S201WP { luminousIntensity, timestamp, name } -> viewBasic' luminousIntensity
+                Sensor.MAX4466 { acousticIntensity, timestamp, name } -> viewBasic' acousticIntensity
+                Sensor.D6T44L06 { temperatures, timestamp, name } -> flow right 
+                    <| List.map viewBasic' temperatures
+                Sensor.HMC5883 { magneticField, timestamp, name } -> viewPoint' magneticField
         in
-            container windowWidth windowHeight (viewPos sensor) e
+            container windowWidth windowHeight (viewPos (windowWidth, windowHeight) imageDimensions sensor) element
     Nothing -> leftAligned (fromString "Sensor data parse error.")
 
-viewPos sensor = 
+viewPos : (Int, Int) -> (Int, Int) -> Sensor.SensorData -> Position
+viewPos (windowWidth, windowHeight) (imageWidth, imageHeight) sensor = 
     let
-        { w, h } = case sensor of
-            Sensor.RHT03 _        -> { w = -0.09,  h = -0.25 }
-            Sensor.TMP421 _       -> { w = 0.09,   h = 0.03 }
-            Sensor.BMP180 _       -> { w = 0.12,   h = 0.1 }
-            Sensor.MLX90614ESF _  -> { w = 0.09,   h = 0.17 }
-            Sensor.SHT15 _        -> { w = -0.095, h = -0.15 }
-            Sensor.PDV_P8104 _    -> { w = -0.11,  h = -0.065 }
-            Sensor.HIH6130 _      -> { w = -0.117, h = 0.14 }
-            Sensor.D6T44L06 _     -> { w = -0.455, h = 0.19 }
-            Sensor.DS18B20 _      -> { w = 0.09,   h = -0.2 }
-            _                     -> { w = 0,      h = 0 }
+        gutter = 10
+        midHeight = (windowHeight // 2)
+        w = (windowWidth // 2) + (imageWidth // 2) + gutter
+        h = case sensor of
+            -- Left
+            Sensor.RHT03 _        -> -180
+            Sensor.SHT15 _        -> -130
+            Sensor.PDV_P8104 _    -> -60
+            Sensor.HIH4030 _      -> 35
+            Sensor.HIH6130 _      -> 75
+            Sensor.D6T44L06 _     -> 107
+            Sensor.TMP102 _       -> 138
+            Sensor.HMC5883 _      -> 195
+
+            -- Right
+            Sensor.DS18B20 _      -> -150
+            Sensor.PR103J2 _      -> -95
+            Sensor.GA1A1S201WP _  -> -65
+            Sensor.SHT75 _        -> -35
+            Sensor.TMP421 _       -> 10 
+            Sensor.BMP180 _       -> 40
+            Sensor.MLX90614ESF _  -> 95
+            Sensor.HTU21D _       -> 130
+            Sensor.MMA8452Q _     -> 195
+            Sensor.MAX4466 _      -> -25
     in
         case side sensor of
-            Left -> midRightAt (absolute 0) (absolute 0)
-            Right -> midLeftAt (absolute 0) (absolute 0)
+            Left -> midRightAt (absolute w) (absolute <| midHeight + h)
+            Right -> midLeftAt (absolute w) (absolute <| midHeight + h)
 
-viewBasic : { value : String, units : String } -> Element
-viewBasic { value, units } = leftAligned (fromString (value ++ " " ++ units))
+viewBasic : Side -> { value : String, units : String } -> Element
+viewBasic side { value, units } = case side of
+    Left -> rightAligned (fromString (value ++ " " ++ units))
+    Right -> leftAligned (fromString (value ++ " " ++ units))
 
 side : Sensor.SensorData -> Side
 side sensor = case sensor of
@@ -131,5 +151,7 @@ side sensor = case sensor of
     Sensor.D6T44L06 _ -> Left
     Sensor.HMC5883 _ -> Left
 
-viewPoint : { x : { value : String, units : String }, y : { value : String, units : String } } -> Element
-viewPoint { x, y } = flow right [viewBasic x, viewBasic y]
+viewPoint : Side 
+    -> { x : { value : String, units : String }, y : { value : String, units : String } } 
+    -> Element
+viewPoint side { x, y } = flow right [viewBasic side x, viewBasic side y]
