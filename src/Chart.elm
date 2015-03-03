d94768ec41c1a3eee4562dab6f0128818720d131
Justin Manley <manleyjster@gmail.com>
2015-03-03 01:17:26 -0600

    Write function for creating real-time line charts from signals of floats.

--- src/Chart.elm
+++ src/Chart.elm
@@ -1,2 +1,16 @@
 module Chart where
-    
+
+import Graphics.Element (Element, empty)
+import Graphics.Collage (path, traced, solid, collage)
+import Color (black)
+import Signal (Signal, map, constant)
+import Signal.Extra (runBuffer)
+import List (map2, length)
+
+chart : Int -> Int -> Signal Float -> Signal Element
+chart width height = map (chart' width height) << runBuffer 60
+
+chart' : Int -> Int -> (List Float) -> Element
+chart' width height data = 
+    let coordinates = map2 (,) [0..toFloat (length data) - 1] data
+    in collage width height [traced (solid black) (path coordinates)]
