ca830d79ce6b5ee05fb75fb33cd54a119cc0fee4
Justin Manley <manleyjster@gmail.com>
2015-03-04 15:14:54 -0600

    Import Waggle.Sensor without qualification.

--- Demo.elm
+++ Demo.elm
@@ -15,7 +15,7 @@ import List
 import Maybe
 import String
 
-import Waggle.Sensor
+import Waggle.Sensor (..)
 import Waggle.Parse
 import Waggle.Layout (Side, side, pos, align)
 import Chart
@@ -35,18 +35,18 @@ main = Signal.map2 view Window.dimensions (take 60 <| currentSensorData)
 ticks : Signal Time
 ticks = every (1 * second)
     
-currentSensorData : Signal (List (Maybe Waggle.Sensor.Sensor))
+currentSensorData : Signal (List (Maybe Sensor))
 currentSensorData = Http.sendGet (Signal.sampleOn ticks (Signal.constant sensorDataUrl)) 
     |> Signal.map handleResponse
 
-handleResponse : Http.Response String -> List (Maybe Waggle.Sensor.Sensor)
+handleResponse : Http.Response String -> List (Maybe Sensor)
 handleResponse response = case response of
     Http.Success str -> Waggle.Parse.parse str
     Http.Waiting -> []
     Http.Failure err msg -> []
 
 -- view
-view : (Int, Int) -> List (List (Maybe Waggle.Sensor.Sensor)) -> Element
+view : (Int, Int) -> List (List (Maybe Sensor)) -> Element
 view (windowWidth, windowHeight) data = 
     let 
         innerWidth = min 980 windowWidth
@@ -68,42 +68,42 @@ view (windowWidth, windowHeight) data =
                 {- end case -})
         ]
 
-viewSensor : (Int, Int) -> (Int, Int) -> Maybe Waggle.Sensor.Sensor -> Element
+viewSensor : (Int, Int) -> (Int, Int) -> Maybe Sensor -> Element
 viewSensor (windowWidth, windowHeight) imageDimensions maybeSensor = case maybeSensor of 
     Just sensor -> 
         let viewBasic' = viewBasic (side sensor)
             viewPoint' = viewPoint (side sensor)
             element = case sensor of
-                Waggle.Sensor.MLX90614ESF { temperature, timestamp, name } -> viewBasic' temperature
-                Waggle.Sensor.TMP421 { temperature, timestamp, name } -> viewBasic' temperature
-                Waggle.Sensor.BMP180 { temperature, pressure, timestamp, name } -> flow down
+                MLX90614ESF { temperature, timestamp, name } -> viewBasic' temperature
+                TMP421 { temperature, timestamp, name } -> viewBasic' temperature
+                BMP180 { temperature, pressure, timestamp, name } -> flow down
                     <| List.map viewBasic' [temperature, pressure]
-                Waggle.Sensor.MMA8452Q { acceleration, timestamp, name } -> viewPoint' acceleration
-                Waggle.Sensor.PDV_P8104 { luminousIntensity, timestamp, name } -> viewBasic' luminousIntensity
-                Waggle.Sensor.PR103J2 { temperature, timestamp, name } -> viewBasic' temperature
-                Waggle.Sensor.HIH6130 { temperature, humidity, timestamp, name } -> flow down 
+                MMA8452Q { acceleration, timestamp, name } -> viewPoint' acceleration
+                PDV_P8104 { luminousIntensity, timestamp, name } -> viewBasic' luminousIntensity
+                PR103J2 { temperature, timestamp, name } -> viewBasic' temperature
+                HIH6130 { temperature, humidity, timestamp, name } -> flow down 
                     <| List.map viewBasic' [temperature, humidity]
-                Waggle.Sensor.SHT15 { temperature, humidity, timestamp, name } -> flow down
+                SHT15 { temperature, humidity, timestamp, name } -> flow down
                     <| List.map viewBasic' [temperature, humidity]
-                Waggle.Sensor.HTU21D { temperature, humidity, timestamp, name } -> flow down 
+                HTU21D { temperature, humidity, timestamp, name } -> flow down 
                     <| List.map viewBasic' [temperature, humidity]
-                Waggle.Sensor.DS18B20 { temperature, timestamp, name } -> viewBasic' temperature
-                Waggle.Sensor.RHT03 { temperature, humidity, timestamp, name } -> flow down
+                DS18B20 { temperature, timestamp, name } -> viewBasic' temperature
+                RHT03 { temperature, humidity, timestamp, name } -> flow down
                     <| List.map viewBasic' [temperature, humidity]
-                Waggle.Sensor.TMP102 { temperature, timestamp, name } -> viewBasic' temperature
-                Waggle.Sensor.SHT75 { temperature, humidity, timestamp, name } -> flow down 
+                TMP102 { temperature, timestamp, name } -> viewBasic' temperature
+                SHT75 { temperature, humidity, timestamp, name } -> flow down 
                     <| List.map viewBasic' [temperature, humidity]
-                Waggle.Sensor.HIH4030 { humidity, timestamp, name } -> viewBasic' humidity
-                Waggle.Sensor.GA1A1S201WP { luminousIntensity, timestamp, name } -> viewBasic' luminousIntensity
-                Waggle.Sensor.MAX4466 { acousticIntensity, timestamp, name } -> viewBasic' acousticIntensity
-                Waggle.Sensor.D6T44L06 { temperatures, timestamp, name } -> flow right 
+                HIH4030 { humidity, timestamp, name } -> viewBasic' humidity
+                GA1A1S201WP { luminousIntensity, timestamp, name } -> viewBasic' luminousIntensity
+                MAX4466 { acousticIntensity, timestamp, name } -> viewBasic' acousticIntensity
+                D6T44L06 { temperatures, timestamp, name } -> flow right 
                     <| List.map viewBasic' temperatures
-                Waggle.Sensor.HMC5883 { magneticField, timestamp, name } -> viewPoint' magneticField
+                HMC5883 { magneticField, timestamp, name } -> viewPoint' magneticField
         in
             container windowWidth windowHeight (pos (windowWidth, windowHeight) imageDimensions sensor) element
     Nothing -> leftAligned (fromString "Sensor data parse error.")
 
-viewBasic : Side -> Waggle.Sensor.Value -> Element
+viewBasic : Side -> Value -> Element
 viewBasic side { value, units } = (align side)
         <| (height 26 <| typeface ["EB Garamond", "serif"] <| fromString (toString value ++ " ")) ++ (viewUnits units)
 
@@ -116,5 +116,5 @@ viewUnits units = height 14
         _   -> units
         {- end case -})
 
-viewPoint : Side -> { x : Waggle.Sensor.Value, y : Waggle.Sensor.Value } -> Element
+viewPoint : Side -> { x : Value, y : Value } -> Element
 viewPoint side { x, y } = flow down [viewBasic side x, viewBasic side y]
