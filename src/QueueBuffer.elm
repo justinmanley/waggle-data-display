046fdd579a63d5bdbe27296cf66d29bc2464ec0f
Justin Manley <manleyjster@gmail.com>
2015-06-09 23:14:42 -0500

    Use foldl instead of foldr for QueueBuffer.fromList (foldr was putting the list into the QueueBuffer in the wrong order).

--- src/QueueBuffer.elm
+++ src/QueueBuffer.elm
@@ -28,7 +28,7 @@ toList buf = Queue.toList buf.queue
 -- TODO: Replace with a more efficient implementation.
 -- This will require adding fromList to the Queue library.
 fromList : Int -> List a -> QueueBuffer a
-fromList bufSize = List.foldr push (empty bufSize)
+fromList bufSize = List.foldl push (empty bufSize)
 
 maxSize : QueueBuffer a -> Int
 maxSize buf = buf.available + Queue.length buf.queue
@@ -39,10 +39,10 @@ last = mapLast Just Nothing
 mapLast : (a -> b) -> b -> QueueBuffer a -> b
 mapLast f default buf = case buf.queue of 
     Queue h t -> case t of
+        last :: _     -> f last
         [] -> case h of
             []        -> default
             last :: _ -> f last
-        last :: _     -> f last
 
 map : (a -> b) -> QueueBuffer a -> QueueBuffer b
 map f buf = { buf | queue <- Queue.map f buf.queue }
